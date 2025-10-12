6f672e4cb5793c1ef998551db8378efed3323b06
sajad yoousf <adilu624@gmail.com>
2025-10-12 12:17:07 +0530

    chagned the color

--- src/Service/Base_url.jsx
+++ src/Service/Base_url.jsx
@@ -2,10 +2,4 @@
 // export const Base_url = "https://spabackend-0tko.onrender.com/api/v1";
 
 // export const Base_url = "http://localhost:5000/api/v1"
-// export const Base_url = "https://spabacklat.onrender.com/api/v1";
 export const Base_url = "https://api.alloraspadubai.com/api/v1";
-// For local development, uncomment the line below and comment the line above
-// export const Base_url = "https://spamanagment-0tko.onrender.com";
-
-// IMPORTANT: Do not use localhost in production. Always use your deployed backend URL above. 
-//something i changed
